fba2eb5c418c5e9504bd086a1dcbf5cb38bba60c
Christina_Sereday <christina.sereday@gmail.com>
2023-11-22 21:11:00 -0500

    Added click on repo functionality

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 const user = "trendingUpward83";
 const profileInfo = document.querySelector(".overview") /*Div w/ profile info */
 const repoList = document.querySelector(".repo-list"); /*Repo list */
+const allRepos = document.querySelector(".repos"); /*section of repos */
+const repoData = document.querySelector(".repo-data"); /*individual repos*/
 
 
 
@@ -37,7 +39,6 @@ const displayUserInfo = function (jsonData) {
 const getRepoInfo = async function () {
     const res = await fetch(`https://api.github.com/users/${user}/repos?sort=updated&per_page=100`);
     const data = await res.json();
-    console.log(data);
     displayRepoInfo(data); 
 };
 
@@ -53,6 +54,41 @@ const displayRepoInfo = function(repos){
 
 getGithubUserInfo();
 
+
+const repoListener = repoList.addEventListener("click", function(e){
+    
+    if (e.target.matches("h3")){
+        const innerName = e.target.innerText;
+        getSpecRepo(innerName); 
+    }
+});
+
+const getSpecRepo = async function(repoName){
+    const res = await fetch(`https://api.github.com/repos/${user}/${repoName}`);
+    const repoInfo = await res.json();
+    const lang = await fetch (`${repoInfo.languages_url}`);
+    const languageData = await lang.json();
+    const languages = [];
+    for (let language in languageData){
+        languages.push(language); 
+    }
+    dispRepoInfo(repoInfo,languages); 
+
+};
+
+const dispRepoInfo = function(repoInfo,languages){
+    repoData.innerHTML="";
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML =`<h3>Name: ${repoInfo.name}</h3>
+    <p>Description: ${repoInfo.description}</p>
+    <p>Default Branch: ${repoInfo.default_branch}</p>
+    <p>Languages: ${languages.join(", ")}</p>
+    <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>`;
+    repoData.append(newDiv);
+    allRepos.classList.add("hide");
+    repoData.classList.remove("hide"); 
+ 
+};
 
 
 
